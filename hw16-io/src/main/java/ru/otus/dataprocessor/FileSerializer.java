2e6fc86dc587eceb58f6d7aca306b3a4a7c3f898
Nail Zinnurov <>
2023-10-26 00:22:00 +0300

    hw-16 io

--- hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,27 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
-    public FileSerializer(String fileName) {}
+    private final String fileName;
+
+    public FileSerializer(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
         // формирует результирующий json и сохраняет его в файл
+        Gson gson = new Gson();
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            gson.toJson(data, fileWriter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,41 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
-import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import ru.otus.model.Measurement;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private static final Type MEASUREMENT_TYPE = new TypeToken<List<Measurement>>() {
+    }.getType();
+
+    private final String fileName;
+
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+    }
 
     @Override
     public List<Measurement> load() {
         // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        try (JsonReader reader =
+                     new JsonReader(
+                             new FileReader(
+                                     getClass()
+                                             .getClassLoader()
+                                             .getResource(fileName)
+                                             .toURI()
+                                             .getPath()))) {
+            return new Gson().fromJson(reader, MEASUREMENT_TYPE);
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
